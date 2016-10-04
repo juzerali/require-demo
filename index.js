0f704a277e0eb323bc1bffb05d092e0482095b7c
Juzer Ali <juzali@cisco.com>
2016-10-04 11:09:30 +0530

    package with require demo

---
+++ index.js
@@ -0,0 +1,4 @@
+module.exports.sum = require("./src/core");
+module.exports.divide = require("./src/extensions/divide");
+module.exports.multiply = require("./src/extensions/multiply");
+module.exports.subtract = require("./src/extensions/subtract");
